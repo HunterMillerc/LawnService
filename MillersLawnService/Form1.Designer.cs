f8cb6d6284ab115903fef827e557f57eb64c6ed8
HunterMillerc <miller.hunterc@gmail.com>
2020-08-07 19:26:52 -0500

    Added all buttons to main form

--- MillersLawnService/Form1.Designer.cs
+++ MillersLawnService/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MillersLawnService
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.mainFrmPicture = new System.Windows.Forms.PictureBox();
+            this.txtMainFrmTitle = new System.Windows.Forms.TextBox();
+            this.btnMainMnuCustomers = new System.Windows.Forms.Button();
+            this.btnMainMnuInvoices = new System.Windows.Forms.Button();
+            this.btnMainMnuInvoiceLineItems = new System.Windows.Forms.Button();
+            this.btnMainMnuServices = new System.Windows.Forms.Button();
+            this.MainMnuEmployees = new System.Windows.Forms.Button();
+            this.btnMainMnuVendors = new System.Windows.Forms.Button();
+            this.btnMainMnuPurchaseOrders = new System.Windows.Forms.Button();
+            this.btnMainMnuPOLineItems = new System.Windows.Forms.Button();
+            this.btnMainMnuExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainFrmPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainFrmPicture
+            // 
+            this.mainFrmPicture.Image = ((System.Drawing.Image)(resources.GetObject("mainFrmPicture.Image")));
+            this.mainFrmPicture.Location = new System.Drawing.Point(280, 12);
+            this.mainFrmPicture.Name = "mainFrmPicture";
+            this.mainFrmPicture.Size = new System.Drawing.Size(263, 239);
+            this.mainFrmPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainFrmPicture.TabIndex = 0;
+            this.mainFrmPicture.TabStop = false;
+            // 
+            // txtMainFrmTitle
+            // 
+            this.txtMainFrmTitle.BackColor = System.Drawing.SystemColors.Control;
+            this.txtMainFrmTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMainFrmTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMainFrmTitle.Location = new System.Drawing.Point(233, 273);
+            this.txtMainFrmTitle.Name = "txtMainFrmTitle";
+            this.txtMainFrmTitle.ReadOnly = true;
+            this.txtMainFrmTitle.Size = new System.Drawing.Size(375, 42);
+            this.txtMainFrmTitle.TabIndex = 1;
+            this.txtMainFrmTitle.Text = "Miller\'s Lawn Service";
+            this.txtMainFrmTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnMainMnuCustomers
+            // 
+            this.btnMainMnuCustomers.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuCustomers.Location = new System.Drawing.Point(22, 339);
+            this.btnMainMnuCustomers.Name = "btnMainMnuCustomers";
+            this.btnMainMnuCustomers.Size = new System.Drawing.Size(172, 57);
+            this.btnMainMnuCustomers.TabIndex = 2;
+            this.btnMainMnuCustomers.Text = "Customers";
+            this.btnMainMnuCustomers.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuInvoices
+            // 
+            this.btnMainMnuInvoices.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuInvoices.Location = new System.Drawing.Point(541, 421);
+            this.btnMainMnuInvoices.Name = "btnMainMnuInvoices";
+            this.btnMainMnuInvoices.Size = new System.Drawing.Size(172, 57);
+            this.btnMainMnuInvoices.TabIndex = 3;
+            this.btnMainMnuInvoices.Text = "Invoices";
+            this.btnMainMnuInvoices.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuInvoiceLineItems
+            // 
+            this.btnMainMnuInvoiceLineItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuInvoiceLineItems.Location = new System.Drawing.Point(502, 497);
+            this.btnMainMnuInvoiceLineItems.Name = "btnMainMnuInvoiceLineItems";
+            this.btnMainMnuInvoiceLineItems.Size = new System.Drawing.Size(243, 57);
+            this.btnMainMnuInvoiceLineItems.TabIndex = 4;
+            this.btnMainMnuInvoiceLineItems.Text = "Invoice Line Items";
+            this.btnMainMnuInvoiceLineItems.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuServices
+            // 
+            this.btnMainMnuServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuServices.Location = new System.Drawing.Point(233, 339);
+            this.btnMainMnuServices.Name = "btnMainMnuServices";
+            this.btnMainMnuServices.Size = new System.Drawing.Size(172, 57);
+            this.btnMainMnuServices.TabIndex = 5;
+            this.btnMainMnuServices.Text = "Services";
+            this.btnMainMnuServices.UseVisualStyleBackColor = true;
+            // 
+            // MainMnuEmployees
+            // 
+            this.MainMnuEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMnuEmployees.Location = new System.Drawing.Point(436, 339);
+            this.MainMnuEmployees.Name = "MainMnuEmployees";
+            this.MainMnuEmployees.Size = new System.Drawing.Size(172, 57);
+            this.MainMnuEmployees.TabIndex = 6;
+            this.MainMnuEmployees.Text = "Employees";
+            this.MainMnuEmployees.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuVendors
+            // 
+            this.btnMainMnuVendors.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuVendors.Location = new System.Drawing.Point(647, 339);
+            this.btnMainMnuVendors.Name = "btnMainMnuVendors";
+            this.btnMainMnuVendors.Size = new System.Drawing.Size(172, 57);
+            this.btnMainMnuVendors.TabIndex = 7;
+            this.btnMainMnuVendors.Text = "Vendors";
+            this.btnMainMnuVendors.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuPurchaseOrders
+            // 
+            this.btnMainMnuPurchaseOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuPurchaseOrders.Location = new System.Drawing.Point(107, 421);
+            this.btnMainMnuPurchaseOrders.Name = "btnMainMnuPurchaseOrders";
+            this.btnMainMnuPurchaseOrders.Size = new System.Drawing.Size(220, 57);
+            this.btnMainMnuPurchaseOrders.TabIndex = 8;
+            this.btnMainMnuPurchaseOrders.Text = "Purchase Orders";
+            this.btnMainMnuPurchaseOrders.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuPOLineItems
+            // 
+            this.btnMainMnuPOLineItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuPOLineItems.Location = new System.Drawing.Point(126, 497);
+            this.btnMainMnuPOLineItems.Name = "btnMainMnuPOLineItems";
+            this.btnMainMnuPOLineItems.Size = new System.Drawing.Size(172, 57);
+            this.btnMainMnuPOLineItems.TabIndex = 9;
+            this.btnMainMnuPOLineItems.Text = "PO Line Items";
+            this.btnMainMnuPOLineItems.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMnuExit
+            // 
+            this.btnMainMnuExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMnuExit.Location = new System.Drawing.Point(647, 596);
+            this.btnMainMnuExit.Name = "btnMainMnuExit";
+            this.btnMainMnuExit.Size = new System.Drawing.Size(172, 57);
+            this.btnMainMnuExit.TabIndex = 10;
+            this.btnMainMnuExit.Text = "&Exit";
+            this.btnMainMnuExit.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(846, 665);
+            this.Controls.Add(this.btnMainMnuExit);
+            this.Controls.Add(this.btnMainMnuPOLineItems);
+            this.Controls.Add(this.btnMainMnuPurchaseOrders);
+            this.Controls.Add(this.btnMainMnuVendors);
+            this.Controls.Add(this.MainMnuEmployees);
+            this.Controls.Add(this.btnMainMnuServices);
+            this.Controls.Add(this.btnMainMnuInvoiceLineItems);
+            this.Controls.Add(this.btnMainMnuInvoices);
+            this.Controls.Add(this.btnMainMnuCustomers);
+            this.Controls.Add(this.txtMainFrmTitle);
+            this.Controls.Add(this.mainFrmPicture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Miller\'s Lawn Service";
+            ((System.ComponentModel.ISupportInitialize)(this.mainFrmPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox mainFrmPicture;
+        private System.Windows.Forms.TextBox txtMainFrmTitle;
+        private System.Windows.Forms.Button btnMainMnuCustomers;
+        private System.Windows.Forms.Button btnMainMnuInvoices;
+        private System.Windows.Forms.Button btnMainMnuInvoiceLineItems;
+        private System.Windows.Forms.Button btnMainMnuServices;
+        private System.Windows.Forms.Button MainMnuEmployees;
+        private System.Windows.Forms.Button btnMainMnuVendors;
+        private System.Windows.Forms.Button btnMainMnuPurchaseOrders;
+        private System.Windows.Forms.Button btnMainMnuPOLineItems;
+        private System.Windows.Forms.Button btnMainMnuExit;
     }
 }
 
